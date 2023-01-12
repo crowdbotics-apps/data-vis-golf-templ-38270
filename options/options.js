b2a5aad9f11b5838bf518d4becc36d9506ac6381
Crowdbotics <team@crowdbotics.com>
2023-01-12 13:53:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "data-vis-golf-templ-38270",
+  url: "https://data-vis-golf-templ-38270.botics.co",
+  api: "https://data-vis-golf-templ-38270.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
